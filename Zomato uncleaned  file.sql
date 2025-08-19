a3d19ba063e807508422445f6e3108108caa8d5a
Rohit Raj Singh <rohit517929@gmail.com>
2025-08-19 17:19:08 +0530

    Update Zomato uncleaned  file.sql

--- Zomato uncleaned  file.sql
+++ Zomato uncleaned  file.sql
@@ -2,4 +2,5 @@ Zomato uncleaned file download link
 
 https://www.kaggle.com/datasets/rishikeshkonapure/zomato
 
-**Link:** [Download file from here](https://www.kaggle.com/datasets/rishikeshkonapure/zomato)
+
+
